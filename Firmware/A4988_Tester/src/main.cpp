eb6a20415033b53c4dffe0bea473c645a875eb74
Ramb0t <rambo@rambo.co.za>
2017-09-19 18:17:11 +0200

    Testing microstepping theory

--- Firmware/A4988_Tester/src/main.cpp
+++ Firmware/A4988_Tester/src/main.cpp
@@ -135,6 +135,7 @@ void setup() {
     // setup serial port
     Serial.begin(115200);
     Serial.println("Welcome to the stepper tester! :D");
+    Serial.println("Constant 16 step mode test");
     Serial.println();
 
     /*
@@ -143,7 +144,7 @@ void setup() {
      * Too high will result in a high pitched whine and the motor does not move.
      */
     stepper.begin(120);
-    stepper.setMicrostep(1); // make sure we are in full speed mode
+    stepper.setMicrostep(16); // make sure we are in full speed mode
     stepper.disable();
 }
 
@@ -158,237 +159,40 @@ void loop() {
     while((digitalRead(btn1)) && (Serial.available() == 0)){
       // do nothing / twiddle thumbs!
     }
+
+    // turn motor on
+    stepper.enable();
+
     strip.setPixelColor(0, BLUE);
     strip.show();
+    delay(50);
 
-    // Tests that we want to run:
-    // 1 disable driver and check outputs low
-    Serial.println("*********************************************");
-    Serial.println("Test 1: Disabling driver and checking outputs");
-    stepper.disable();
-    delay(10); // wait a bit for outputs to settle
-    checkDriverOutputs();
-    printDriverOStates();
-
-    if(s1A || s1B || s2A || s2B){// one of the states is high, we have a problem!
-      flagPass = false;
-      Serial.println("Error! All states should be low");
-      Serial.println("Likely we have a faulty output stage, or wiring");
-      Serial.println();
-    }else{
-      Serial.println("Test 1: Complete");
-      Serial.println();
-    }
-
-    strip.setPixelColor(0, MAGENTA);
-    strip.show();
-    delay(100);
-    strip.setPixelColor(0, BLUE);
-    strip.show();
-
-    // 2 enable and check output works, see what state we are in?
-    if(flagPass){ // skip test, failed
-      Serial.println("*********************************************");
-      Serial.println("Test 2: Enabling driver and checking outputs");
-      stepper.enable();
-      stepper.setMicrostep(1); // make sure we are in full speed / step mode
-      delay(10); // wait a bit for outputs to settle
-
-      //delay(1000);
-      checkDriverOutputs(); //0
-      printDriverOStates();
-
-      if(!s1A && !s1B && !s2A && !s2B){// one of the states is high, we have a problem!
-        flagPass = false;
-        Serial.println("Error! Not all states should be low");
-        Serial.println("Likely we have a faulty output stage, or wiring");
-        Serial.println();
+    while(flagPass){
+      // step forward 1 rotation
+      for(int i = 0 ; i < 200 ; i++){
+        // check for interrupt from button
+        if(!(digitalRead(btn1))){
+          flagPass = false; // time to exit...
+        }
+        if(!flagPass){
+          break; // get out of for loop
+        }
+        stepper.move(1);
       }
 
-      stepper.move(1);
-      //delay(1000);
-      checkDriverOutputs(); //1
-      printDriverOStates();
-
-      stepper.move(1);
-      //delay(1000);
-      checkDriverOutputs(); //2
-      printDriverOStates();
-
-      stepper.move(1);
-      //delay(1000);
-      checkDriverOutputs(); //3
-      printDriverOStates();
-
-      stepper.move(1);
-      //delay(1000);
-      checkDriverOutputs(); //0
-      printDriverOStates();
-
-      stepper.move(1);
-      //delay(1000);
-      checkDriverOutputs(); //1
-      printDriverOStates();
-
-      strip.setPixelColor(0, MAGENTA);
-      strip.show();
-      delay(100);
-      strip.setPixelColor(0, BLUE);
-      strip.show();
-
-      Serial.println("Test 2: Now checking reverse");
-
-      stepper.move(-1);
-      checkDriverOutputs(); //3
-      printDriverOStates();
-
-      stepper.move(-1);
-      checkDriverOutputs(); //2
-      printDriverOStates();
-
-      stepper.move(-1);
-      checkDriverOutputs(); //1
-      printDriverOStates();
-
-      stepper.move(-1);
-      checkDriverOutputs(); //0
-      printDriverOStates();
-
-      stepper.move(-1);
-      checkDriverOutputs(); //3
-      printDriverOStates();
-
-      stepper.move(-1);
-      checkDriverOutputs(); //2
-      printDriverOStates();
-
-      //TODO: Build driver state truth table
-
-      Serial.println("Test 2: Complete");
-      Serial.println();
-
-      strip.setPixelColor(0, MAGENTA);
-      strip.show();
-      delay(100);
-      strip.setPixelColor(0, BLUE);
-      strip.show();
-    }
-
-    // 3 step 4 times CW and check states
-    if(flagPass){ // skip test, failed
-      Serial.println("*********************************************");
-      Serial.println("Test 3: Do 4 full rotations");
-      for(int l = 0 ; l<4 ; l++){
-        stepper.rotate(360);
-        stepper.rotate(-360);
+      // step back 1 rotation
+      for(int i = 0 ; i < 200 ; i++){
+        // check for interrupt from button
+        if(!(digitalRead(btn1))){
+          flagPass = false; // time to exit...
+        }
+        if(!flagPass){
+          break; // get out of for loop
+        }
+        stepper.move(-1);
       }
-
-      Serial.println("Test 3: Complete");
-      Serial.println();
-
-      strip.setPixelColor(0, MAGENTA);
-      strip.show();
-      delay(100);
-      strip.setPixelColor(0, BLUE);
-      strip.show();
     }
-
-
-    // 4 repeat for 2, 4, 8, 16 MS
-    if(flagPass){ // skip test, failed
-      Serial.println("*********************************************");
-      Serial.println("Test 4: Change MS 2,4,8,16 and rotate");
-      Serial.println("Test 4: 2 MicroSteps...");
-      stepper.setMicrostep(2);
-      for(int l = 0 ; l<4 ; l++){
-        stepper.rotate(360);
-        stepper.rotate(-360);
-      }
-
-      strip.setPixelColor(0, MAGENTA);
-      strip.show();
-      delay(100);
-      strip.setPixelColor(0, BLUE);
-      strip.show();
-
-      Serial.println("Test 4: 4 MicroSteps...");
-      stepper.setMicrostep(4);
-      for(int l = 0 ; l<4 ; l++){
-        stepper.rotate(360);
-        stepper.rotate(-360);
-      }
-
-      strip.setPixelColor(0, MAGENTA);
-      strip.show();
-      delay(100);
-      strip.setPixelColor(0, BLUE);
-      strip.show();
-
-      Serial.println("Test 4: 8 MicroSteps...");
-      stepper.setMicrostep(8);
-      for(int l = 0 ; l<4 ; l++){
-        stepper.rotate(360);
-        stepper.rotate(-360);
-      }
-
-      strip.setPixelColor(0, MAGENTA);
-      strip.show();
-      delay(100);
-      strip.setPixelColor(0, BLUE);
-      strip.show();
-
-      Serial.println("Test 4: 16 MicroSteps...");
-      stepper.setMicrostep(16);
-      for(int l = 0 ; l<4 ; l++){
-        stepper.rotate(360);
-        stepper.rotate(-360);
-      }
-
-      Serial.println("Test 4: Complete");
-      Serial.println();
-
-      strip.setPixelColor(0, MAGENTA);
-      strip.show();
-      delay(100);
-      strip.setPixelColor(0, BLUE);
-      strip.show();
-
-      // 5 check states for above modes
-      // 6 check states again
-      // 7 disable, check states, try step
-    }
-
-    if(flagPass){
-      strip.setPixelColor(0, GREEN);
-      strip.show();
-      Serial.println("Tests Complete! ");
-    }else{
-      strip.setPixelColor(0, RED);
-      strip.show();
-      Serial.println("Tests Failed! ");
-    }
-
-    Serial.println("*********************************************");
-    Serial.println("*********************************************");
-    Serial.println();
-    Serial.println();
-    Serial.println();
-
-    stepper.disable();
-
-    Serial.println("Press button or send 's' to restart test...");
-
-    while((digitalRead(btn1)) && (Serial.available() == 0)){
-      // do nothing / twiddle thumbs!
-    }
-    Serial.println("It is now safe to install a new driver");
-    delay(1000);
-
-
-    Serial.read();
-    Serial.flush();
-
-
+    
 }
 
 // function to check the driver outputs
